4eeb8127e4245371207f0f576d6037c788a28e4b
ArtemDerkach <artem.derkach.gs@gmail.com>
2019-06-14 00:15:12 +0300

    add dummy server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("running dbfs....")
+	fmt.Println("running dbfs on :8080")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from DBFS"))
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
