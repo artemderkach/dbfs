af6f899faf93f02e843fec1495209e22f28af1c8
mind-rot <artem.derkach.gs@gmail.com>
2019-06-24 18:01:49 +0300

    add database reading

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	fmt.Println("running dbfs on :8080")
 	http.HandleFunc("/", home)
-	http.HandleFunc("/db", db)
+	http.HandleFunc("/db", initDB)
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
@@ -21,8 +22,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from DBFS"))
 }
 
-func db(w http.ResponseWriter, r *http.Request) {
+func initDB(w http.ResponseWriter, r *http.Request) {
 	db, err := bolt.Open("db.bolt", 0600, nil)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte("files"))
+		if err != nil {
+			return err
+		}
+		b.Put([]byte("test"), []byte("42"))
+		return nil
+	})
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte("some"))
+}
+
+func dbHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := bolt.Open("db.bolt", 0600, nil)
+	if err != nil {
+		panic(err)
+	}
 	if err != nil {
 		panic(err)
 	}
